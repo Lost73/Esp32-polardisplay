1940046d37e595937601fd55a4584040492580cf
Lost73 <62477244+Lost73@users.noreply.github.com>
2020-03-22 01:03:57 +0100

    Update AfficheurExt.ino

--- AfficheurExt.ino
+++ AfficheurExt.ino
@@ -13,9 +13,9 @@
 #include <HTTPClient.h>
 #include <SSD1283A.h> //Hardware-specific library
 #include <Fonts/FreeMonoBoldOblique12pt7b.h>
-#include <Fonts/FreeSerif9pt7b.h>
-
+#include <Fonts/FreeMonoBold18pt7b.h>
 #include <Fonts/FreeMonoBold24pt7b.h>
+
 SSD1283A tft(/*CS=5*/ SS, /*A0DC=*/ 17, /*RST=*/ 16, /*LED=*/ 4); //hardware spi,cs,cd,reset,led SCL=18 SDA=23MOSI
 
 #define  BLACK  0x0000
@@ -28,23 +28,22 @@ SSD1283A tft(/*CS=5*/ SS, /*A0DC=*/ 17, /*RST=*/ 16, /*LED=*/ 4); //hardware spi
 #define WHITE   0xFFFF
 
 // Set your network
-const char* ssid = "yourssid";
-const char* password = "yourpassword";
+const char* ssid = "freebox_HWWVKE";
+const char* password = "virginie75";
 
 //Your IP address or domain name with URL path
-const char* serverNameTemp = "http://192.168.0.35/temperature";
-const char* serverNamePres = "http://192.168.0.35/pressure";
-const char* serverNametarg = "http://192.168.0.35/target";
-const char* serverNametwa = "http://192.168.0.35/twa";
-const char* serverNametws = "http://192.168.0.35/tws";
 
-String temperature;
-String pressure;
-String target;
-String twa;
+const char* serverNameTarg = "http://192.168.0.35/target";
+const char* serverNameTws = "http://192.168.0.35/tws";
+const char* serverNameSog = "http://192.168.0.35/sog";
+const char* serverNameTwa = "http://192.168.0.35/twa";
+
 String tws;
-float flag = 0;
-float i = 0;
+String twa;
+String target;
+String sog;
+int perf;
+
 unsigned long previousMillis = 0;
 const long interval = 1000;
 
@@ -81,86 +80,81 @@ void loop() {
   if (currentMillis - previousMillis >= interval) {
     // Check WiFi connection status
     if (WiFi.status() == WL_CONNECTED ) {
-      temperature = httpGETRequest(serverNameTemp);
-      pressure = httpGETRequest(serverNamePres);
-      target = httpGETRequest(serverNametarg);
-      twa = httpGETRequest(serverNametwa);
-      tws = httpGETRequest(serverNametws);
-if (i>22){
-flag = 0;
-}
- if (i<0){
-flag = 0;
-}
- if (flag==0){
-i=i+0,1;
-}  
- 
- if (flag==1){
-i=i+0,1;
-}    
-      Serial.println("Temperature: " + temperature + " Pressure: " + pressure + " hPa");
+      sog = httpGETRequest(serverNameSog);
+      target = httpGETRequest(serverNameTarg);
+      tws = httpGETRequest(serverNameTws);
+      twa = httpGETRequest(serverNameTwa);
+      Serial.println("sog " + sog +  " perf" + perf + " tgt" + target + " tws" + tws );
 
       tft.fillScreen(WHITE);
 
-      // display temperature
+      // display cog
       tft.setFont();
-      tft.setTextSize(2);
+      tft.setTextSize(1);
       tft.setTextColor(BLACK);
       tft.setCursor(0, 0);
-      tft.print("T:");
-      tft.print(temperature);
-      tft.print(" ");
-      tft.setTextSize(2);
-      //display.cp437(true);
-      tft.write(248);
-      tft.setTextColor(RED);
-      tft.setTextSize(2);
-      tft.print("C");
-
-      // display pressure
-      tft.setTextSize(2);
-      tft.setCursor(0, 16);
-      tft.print("P:");
-      tft.print(pressure);
-      tft.setTextSize(1);
-      tft.setCursor(110, 22);
-      tft.print("hPa");
-
-
-
-
-      // display twa
-      tft.setTextSize(2);
-      tft.setCursor(0, 32);
-      tft.print("Twa:");
-      tft.print(twa);
+      tft.print("sog:");
+      int s = sog.toInt();
+      tft.print(float(s) / 10, 1);
 
 
       // display tws
-      tft.setTextSize(2);
-      tft.setCursor(0, 45);
-      tft.print("Tws:");
-      tft.print(tws);
 
-      // display target
-      tft.setTextColor(GREEN);
+      tft.setCursor(48, 0);
+      tft.print("twa");
+      tft.print(twa);
+
+      tft.setCursor(88, 0);
+      tft.print("tws");
+      tft.print(tws);
+      // display Perf
+      tft.setTextColor(BLUE);
       tft.setTextSize(1);
-      tft.setCursor(0, 66);
-      tft.print("Tgt");
-      tft.setCursor(20, 102);
-      tft.setTextSize(1);
-      //tft.print(target);
-      tft.setFont(&FreeMonoBold24pt7b);
-      tft.print(i,1);
+      tft.setCursor(0, 24);
+      tft.print("perf");
+
+      tft.setCursor(0, 70);
+      tft.setTextSize(2);
+      int t = target.toInt();
+      if (t != 0) {
+        perf = (int(float(s) / float(t) * 100));
+        tft.setFont(&FreeMonoBold18pt7b);
+        if (perf < 100) {
+          tft.print(" ");
+        }
+        tft.print(perf);
+      }
+
       //tft.print(target);
       //display.display();
+
+      // display target
+      tft.setTextColor(BLACK);
+      tft.setTextSize(1);
+      tft.setFont();
+      tft.setCursor(0, 74);
+      tft.print("Tgt");
+      tft.setCursor(0, 118);
+      tft.setTextSize(2);
+      tft.setFont(&FreeMonoBold18pt7b);
+      if (target.toInt() < 100) {
+        tft.print(" ");
+      }
+      tft.print(target);
+      tft.setCursor(64, 126);
+      tft.print(".");
 
       // save the last HTTP GET Request
       previousMillis = currentMillis;
     }
     else {
+      tft.fillScreen(WHITE);
       Serial.println("WiFi Disconnected");
+      tft.setFont();
+      tft.setTextSize(2);
+      tft.setTextColor(BLACK);
+      tft.setCursor(0, 0);
+      tft.print("WiFi Disconnected");
     }
   }
 }
